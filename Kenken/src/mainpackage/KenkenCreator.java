e53507f2e05aba67bc0a0b803e6213eb46cc0b28
Justin <justinbaradi@hotmail.com>
2013-03-20 16:59:55 -0400

    fixed some "cages" bugs
    -added temp console gui for showing cages
    -fixed bug: only generating HBox and VBox

--- Kenken/src/mainpackage/KenkenCreator.java
+++ Kenken/src/mainpackage/KenkenCreator.java
@@ -1,8 +1,12 @@
 package mainpackage;
+
 //import java.util.ArrayList;
+import java.util.Random;
+
 
 public class KenkenCreator {
 	public static int difficulty;
+	public static Random rand = new Random();
 	
 	public static void main(String[] args){
 		difficulty = 6;
@@ -22,7 +26,6 @@ public class KenkenCreator {
 		 */
 		
 		kenken = createPuzzle(kenken); //creates a new puzzle
-		cages = createCages(kenken, 4); //creates the random cages for the puzzle
 		
 		
 		
@@ -33,6 +36,18 @@ public class KenkenCreator {
 			}
 			System.out.println();
 		}
+		System.out.println("");
+		
+		cages = createCages(kenken, 4); //creates the random cages for the puzzle
+
+		System.out.println();
+		for (int i = 0; i < difficulty; i++) {
+			for (int j = 0; j < difficulty; j++) {
+				System.out.print(cages[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
 	}
 	
 	
@@ -73,7 +88,7 @@ public class KenkenCreator {
 		for(int j = 0; j < difficulty; j++) {
 			do { //creates a new number if there is a conflicting number
 				conflict = false;
-				temp = 1 + ((int)(Math.random() * difficulty)); //generates a random number
+				temp = 1 + rand.nextInt(difficulty); //generates a random number
 				for (int chkRow = 0; chkRow < j; chkRow++) {
 					if (newUniqueRow[chkRow] == temp)  //checks if any number is conflicting with the temp number
 						conflict = true;
@@ -106,7 +121,7 @@ public class KenkenCreator {
 		
 		/* String Code for the cages double array:
 		 * 
-		 * Sample: 12+L
+		 * Sample: L+12
 		 * 
 		 * 12 - Number that should be produced by the operator with all numbers in cage
 		 * + - operator to be applied to all numbers in a cage
@@ -133,59 +148,49 @@ public class KenkenCreator {
 					 * 7* - 1Box (NOTE: to be generated last when there is no space for any other shape) 
 					 */
 				
-					if (!(connCells[i][j] == 0)) { //checks if entering a shape in the specified coordinate is possible
+					if (connCells[i][j] == 0) { //checks if entering a shape in the specified coordinate is possible
 						
 						//integers retrieved from the shape and produce a number corresponding to the operator (used later in Step 3)
 						int a = 0, b[] = new int[7], c[] = new int[4], d = 0, z = 0; 
 						a = curPuzzle[i][j]; //a is the current coordinate number from the solution
+						String oper = "+"; //operator stored in a string (used later in Step 3)
+						
 						
 						//Step 1 - Generates a random shape (if possible, otherwise 1Box) to the specified coordinate
 						//checks if its possible to enter a shape there, otherwise the shape will be skipped
 						boolean possShapes[] = new boolean[7];
 						currCageNum++; //increments the current cage it is working on
-						connCells[i][j] = currCageNum; //makes the current cage, from the coordinates, the current cage number
-						
 						if(i + 1 < difficulty) {
 							if(connCells[i+1][j] == 0) { //VBox
 								possShapes[0] = true;
-								connCells[i+1][j] = currCageNum;
 								b[0] = curPuzzle[i+1][j];
 							}
 						}
 						if (j + 1 < difficulty) {
 							if(connCells[i][j+1] == 0) { //HBox
 								possShapes[1] = true;
-								connCells[i][j+1] = currCageNum;
 								b[1] = curPuzzle[i][j+1];
 							}
 						}
 						if ((i + 1) < difficulty && (j + 1) < difficulty) {
 							if(connCells[i][j+1] == 0 && connCells[i+1][j] == 0 && connCells[i+1][j+1] == 0) { //BBox
 								possShapes[2] = true;
-								connCells[i][j+1] = currCageNum;
-								connCells[i+1][j] = currCageNum;
-								connCells[i+1][j+1] = currCageNum;
 								b[2] = curPuzzle[i][j+1];
 								c[0] = curPuzzle[i+1][j];
 								d = curPuzzle[i+1][j+1];
+								System.err.println("I DID IT " + i + " " + j);
 							} else if(connCells[i+1][j+1] == 0 && connCells[i+1][j] == 0) { //LN (L)
 								possShapes[3] = true;
-								connCells[i+1][j+1] = currCageNum;
-								connCells[i+1][j] = currCageNum;
 								b[3] = curPuzzle[i][j+1];
 								c[1] = curPuzzle[i+1][j];
 								
 								
 							} else if(connCells[i+1][j] == 0 && connCells[i][j+1] == 0) { //LNF (T)
 								possShapes[5] = true;
-								connCells[i+1][j] = currCageNum;
-								connCells[i][j+1] = currCageNum;
 								b[5] = curPuzzle[i+1][j];
 								c[3] = curPuzzle[i][j+1];
 							} else if(connCells[i][j+1] == 0 && connCells[i+1][j+1] == 0) { //LBF (7)
 								possShapes[6] = true;
-								connCells[i][j+1] = currCageNum;
-								connCells[i+1][j+1] = currCageNum;
 								b[6] = curPuzzle[i][j+1];
 								c[4] = curPuzzle[i+1][j+1];
 							}
@@ -193,8 +198,6 @@ public class KenkenCreator {
 						if ((i + 1) < difficulty && (j - 1) >= 0) {
 							if(connCells[i+1][j] == 0 && connCells[i+1][j-1] == 0) { //LB (J)
 								possShapes[4] = true;
-								connCells[i+1][j] = currCageNum;
-								connCells[i+1][j-1] = currCageNum;
 								b[4] = curPuzzle[i+1][j];
 								c[2] = curPuzzle[i+1][j-1];
 							}
@@ -210,8 +213,9 @@ public class KenkenCreator {
 						int genShape = 0;
 						if (anyPossShape == true) {
 							do {
-								genShape = (int)(Math.random() * 7); //picks a random shape to do
+								genShape = rand.nextInt(7); //picks a random shape to do
 							} while (possShapes[genShape] == false); //only if that shape is possible
+						//System.err.println(genShape);
 						} else {
 							genShape = 7; //do a 1Box
 						}
@@ -223,7 +227,7 @@ public class KenkenCreator {
 						
 						if ((genShape == 0 || genShape == 1) && numOfOper >= 3) { //currCages is a 2x1 box and the numOfOper = 4 and x / y = an integer
 							//Does a random operation from add, multiply, and subtract, ONLY if division (if numOfOper = 4) does NOT produce an integer
-							intOper = (int)(Math.random() * 3);
+							intOper = rand.nextInt(3);
 							
 							//Checks if an integer is produced when divided
 							if (numOfOper == 4) {
@@ -236,15 +240,26 @@ public class KenkenCreator {
 
 
 								if(x%y==0) //it is an integer
-									intOper = (int)(Math.random() * numOfOper); //gets a random operator from one of the 4 operators (+ * - /)
+									intOper = rand.nextInt(numOfOper); //gets a random operator from one of the 4 operators (+ * - /)
 							}
 						} else {
-							intOper = (int)(Math.random() * 2); //this excludes division and subtraction from being used for the cage
+							intOper = rand.nextInt(2); //this excludes division and subtraction from being used for the cage
 						}
 						
+						if (intOper == 0)
+							oper = "+";
+						else if (intOper == 1)
+							oper = "*";
+						else if (intOper == 2)
+							oper = "-";
+						else if (intOper == 3)
+							oper = "/";
 						
+
 						
 						//Step 3 - Get the number produced from the operator
+						connCells[i][j] = currCageNum; //makes the current cage, from the coordinates, the current cage number
+						
 						if (genShape == 0) { //VBox
 							if (intOper == 0) { //add
 								z = a + b[0];
@@ -253,13 +268,16 @@ public class KenkenCreator {
 							} else if(intOper == 2) { //subtract
 								z = a - b[0];
 								if (z < 0)
-									z = z * 1;
+									z = z * -1;
 							} else if(intOper == 3) { //divide
 								if (a > b[0])
 									z = a / b[0];
 								else
 									z = b[0] / a;
 							}
+							currCages[i][j] = "VBox" + oper + "" + z;
+							currCages[i+1][j] = "VBox" + oper + "" + z;
+							connCells[i+1][j] = currCageNum;
 						} else if(genShape == 1) { //HBox
 							if (intOper == 0) { //add
 								z = a + b[1];
@@ -268,48 +286,77 @@ public class KenkenCreator {
 							} else if(intOper == 2) { //subtract
 								z = a - b[1];
 								if (z < 0)
-									z = z * 1;
+									z = z * -1;
 							} else if(intOper == 3) { //divide
 								if (a > b[1])
 									z = a / b[1];
 								else
 									z = b[1] / a;
 							}
+							currCages[i][j] = "HBox" + oper + "" + z;
+							currCages[i][j+1] = "HBox" + oper + "" + z;
+							connCells[i][j+1] = currCageNum;
 						} else if(genShape == 2) { //BBox
 							if (intOper == 0) { //add
 								z = a + b[2] + c[0] + d;
 							} else if(intOper == 1) { //multiply
 								z = a * b[2] * c[0] * d;
 							}
+							currCages[i][j] = "BBox" + oper + "" + z;
+							currCages[i+1][j] = "BBox" + oper + "" + z;;
+							currCages[i][j+1] = "BBox" + oper + "" + z;;
+							currCages[i+1][j+1] = "BBox" + oper + "" + z;
+							connCells[i][j+1] = currCageNum;
+							connCells[i+1][j] = currCageNum;
+							connCells[i+1][j+1] = currCageNum;
 						} else if(genShape == 3) { //LN
 							if (intOper == 0) { //add
 								z = a + b[3] + c[1];
 							} else if(intOper == 1) { //multiply
 								z = a * b[3] * c[1];
 							}
+							currCages[i][j] = "LN" + oper + "" + z;
+							currCages[i+1][j] = "LN" + oper + "" + z;
+							currCages[i+1][j+1] = "LN" + oper + "" + z;
+							connCells[i+1][j] = currCageNum;
+							connCells[i+1][j+1] = currCageNum;
 						} else if(genShape == 4) { //LB
 							if (intOper == 0) { //add
 								z = a + b[4] + c[2];
 							} else if(intOper == 1) { //multiply
 								z = a * b[4] * c[2];
 							}
+							currCages[i][j] = "LB" + oper + "" + z;
+							currCages[i+1][j] = "LB" + oper + "" + z;
+							currCages[i+1][j-1] = "LB" + oper + "" + z;
+							connCells[i+1][j] = currCageNum;
+							connCells[i+1][j-1] = currCageNum;
 						} else if(genShape == 5) { //LNF
 							if (intOper == 0) { //add
 								z = a + b[5] + c[3];
 							} else if(intOper == 1) { //multiply
 								z = a * b[5] * c[3];
 							}
+							currCages[i][j] = "LNF" + oper + "" + z;
+							currCages[i][j+1] = "LNF" + oper + "" + z;
+							currCages[i+1][j] = "LNF" + oper + "" + z;
+							connCells[i][j+1] = currCageNum;
+							connCells[i+1][j] = currCageNum;
 						} else if(genShape == 6) { //LBF
 							if (intOper == 0) { //add
 								z = a + b[6] + c[4];
 							} else if(intOper == 1) { //multiply
 								z = a * b[6] * c[4];
 							}
+							currCages[i][j] = "LBF" + oper + "" + z;
+							currCages[i][j+1] = "LBF" + oper + "" + z;
+							currCages[i+1][j+1] = "LBF" + oper + "" + z;
+							connCells[i][j+1] = currCageNum;
+							connCells[i+1][j+1] = currCageNum;
 						} else if(genShape == 7) { //1Box
 							z = a;
+							currCages[i][j] = "1Box" + z;
 						}
-						
-						
 					}
 				//}
 			}
