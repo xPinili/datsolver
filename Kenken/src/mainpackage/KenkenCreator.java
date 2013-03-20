cb4b17a02fc25250242ec49b56dafa0b57f5d6ca
Nerman <n.nicholas110@gmail.com>
2013-03-20 19:57:52 -0400

    Kenken Input capabilities in KenkenCreator.java
    Very early stages, and we might not need this when connecting it to the
    GUI.

--- Kenken/src/mainpackage/KenkenCreator.java
+++ Kenken/src/mainpackage/KenkenCreator.java
@@ -1,16 +1,18 @@
-package mainpackage;
+//package mainpackage;
 
 //import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class KenkenCreator {
 	public static int difficulty;
 	public static Random rand = new Random();
+	public static int[][] kenken;
 	
 	public static void main(String[] args){
 		difficulty = 3;
-		int[][] kenken = new int[difficulty][difficulty];
+		kenken = new int[difficulty][difficulty];
 		String[][] cages = new String[difficulty][difficulty];
 		
 		
@@ -362,6 +364,45 @@ public class KenkenCreator {
 		return currCages;
 	}
 	
-	
+	public static void inputProblem(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the difficulty of the puzzle you are about to input: ");
+		difficulty = input.nextInt();
+		
+		System.out.println("How our puzzles are inputted");
+		System.out.println("--------------------------------------------------------------------------------------------");
+		System.out.println("Code for input: [shape][operator][overall value of cell] \n");
+		System.out.println("Acceptable Shapes:");
+		System.out.println("VBox = Vertical Box (2 Boxes going down)");
+		System.out.println("HBox = Horizonal Box (2 Boxes going across)");
+		System.out.println("LN = L shape, that is orientated regularly (takes up 3 boxes)");
+		System.out.println("LB = L shape, that is orientated backwards (takes up 3 boxes)");
+		System.out.println("LNF = L shape, that is orientated regularly and flipped (takes up 3 boxes)");
+		System.out.println("LBF = L shape, that is orientated backwards and flipped (takes up 3 boxes)");
+		System.out.println("1Box = 1 box\n");
+		System.out.println("Acceptable Operators:");
+		System.out.println("+ (Addition)");
+		System.out.println("- (Subtraction)");
+		System.out.println("/ (Division)");
+		System.out.println("* (Multiplication)\n");
+		System.out.println("Accepted Overall Values:");
+		System.out.println("-Has to be the value when all numbers in that cage are calculated with that cages' operator.\n");
+		System.out.println("Sample Input: For a 3 by 3 Puzzle:");
+		System.out.println("VBox-1 -> VBox+3 -> VBox/3 -> VBox-1 -> VBox+3 -> VBox/3 -> HBox+4 -> HBox+4  -> 1Box2 ");
+		System.out.println("--------------------------------------------------------------------------------------------");
+		
+		String storeValue = "";
+		
+		for(int i=0; i<(difficulty); i++){
+			for(int j=0; i<(difficulty); i++){
+				System.out.println("Enter a value for position: " + (i+1) + (j+1) + ": ");
+				storeValue = input.nextLine();
+				
+				
+			}
+		}
+		
+		input.close();
+	}
 	
 }
